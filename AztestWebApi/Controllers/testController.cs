f55d76d86707326bf19319186802923e732cde0d
sarvesh.upadhyay <sarveshupadhayay@tavant.com>
2020-03-01 11:58:03 +0530

    testController is added

---
+++ AztestWebApi/Controllers/testController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AztestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        // GET: api/test
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Azure DevOps", "AWS DevOps" };
+        }
+
+        // GET: api/test/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/test
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/test/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
